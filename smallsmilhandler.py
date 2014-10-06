e41b2849b6a341936eb37620b4f583140e24b177
Miriam Mendoza Zurita <miriammz@iota39.aulas.gsyc.es>
2014-10-06 10:27:30 +0200

    añado fichero

---
+++ smallsmilhandler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+
+class SmallSMILHandler(ContentHandler):
+
+if __name__ == "__main__":
+
+	print hola
