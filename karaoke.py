3747e7950cad5ad0e53b5138a51ce0d1c15b0b59
MIRIAM <miriam@ubuntu.(none)>
2014-10-11 11:50:05 +0200

    creada la clase, el init y el main

--- karaoke.py
+++ karaoke.py
@@ -15,20 +15,20 @@ class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
 		cHandler = smallsmilhandler.SmallSMILHandler()
 		parser.setContentHandler(cHandler)
 		parser.parse(open(smil))	
-		self.lista = cHandler.get_tags()		
+		self.lista = cHandler.get_tags()	
+
+	def __str__(self):  #imprime la lista
+		print "2"
+
+	def do_local(self):	 #descarga recursos remotos
+		print "6"
 
 if __name__ == "__main__":
 	try:	
 		smil = sys.argv[1]
 		karaoke = KaraokeLocal(smil)
-		 #print "hola"
+		karaoke.__str__()
+		karaoke.do_local()
 	except:
 		print "Usage: python karaoke.py file.smil"
 		sys.exit() 
-	
-
-
-		
-		
-
-
