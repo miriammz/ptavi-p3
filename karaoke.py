6e80a12fead6a77f5a002dcabab2b5ea866daf6f
MIRIAM <miriam@ubuntu.(none)>
2014-10-11 14:34:16 +0200

    error al imprimir sin espacios

--- karaoke.py
+++ karaoke.py
@@ -3,32 +3,50 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-import smallsmilhandler 
+import smallsmilhandler
 import sys
 import os
 
 
 class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
 
-	def __init__(self, fichero):
-		parser = make_parser()
-		cHandler = smallsmilhandler.SmallSMILHandler()
-		parser.setContentHandler(cHandler)
-		parser.parse(open(smil))	
-		self.lista = cHandler.get_tags()	
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(smil))
+        self.lista = cHandler.get_tags()
 
-	def __str__(self):  #imprime la lista
-		print "2"
+    def __str__(self):  # imprime la lista
+        for sublista in self.lista:
+            etiqueta = sublista[0]
+            sublista_atrib = sublista[1]
+            print etiqueta + "\t",
+            for atributo in sublista_atrib:
+                if sublista_atrib[atributo] != "":
+                    print atributo + "=", sublista_atrib[atributo], "\t",
+            print
 
-	def do_local(self):	 #descarga recursos remotos
-		print "6"
+    def do_local(self):     # descarga recursos remotos
+        for sublista in self.lista:
+            etiqueta = sublista[0]
+            sublista_atrib = sublista[1]
+            for atributo in sublista_atrib:
+                if sublista_atrib[atributo] != "" and atributo == "src":
+                    recurso = sublista_atrib[atributo]
+                    os.system("wget -q " + recurso)
+                    lista = recurso.split("/")
+                    lista = lista[-1]
+                    sublista_atrib[atributo] = lista
+                    print sublista_atrib[atributo]
 
 if __name__ == "__main__":
-	try:	
-		smil = sys.argv[1]
-		karaoke = KaraokeLocal(smil)
-		karaoke.__str__()
-		karaoke.do_local()
-	except:
-		print "Usage: python karaoke.py file.smil"
-		sys.exit() 
+    try:
+        smil = sys.argv[1]
+        karaoke = KaraokeLocal(smil)
+        karaoke.__str__()
+        karaoke.do_local()
+    except:
+        print "Usage: python karaoke.py file.smil"
+        sys.exit()
+# si quiero imprimir sin espacios entre el "=", me da usage error
