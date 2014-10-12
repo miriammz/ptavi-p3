c17fff5568da4afa7b8eae0631ec6906ebae8989
MIRIAM <miriam@ubuntu.(none)>
2014-10-12 11:52:24 +0200

    terminado, al imprimir problema con el tabulador

--- karaoke.py
+++ karaoke.py
@@ -14,39 +14,38 @@ class KaraokeLocal(smallsmilhandler.SmallSMILHandler):
         parser = make_parser()
         cHandler = smallsmilhandler.SmallSMILHandler()
         parser.setContentHandler(cHandler)
-        parser.parse(open(smil))
+        parser.parse(open(sys.argv[1]))
         self.lista = cHandler.get_tags()
 
     def __str__(self):  # imprime la lista
         for sublista in self.lista:
             etiqueta = sublista[0]
-            sublista_atrib = sublista[1]
+            dic = sublista[1]
             print etiqueta + "\t",
-            for atributo in sublista_atrib:
-                if sublista_atrib[atributo] != "":
-                    print atributo + "=", sublista_atrib[atributo], "\t",
+            for atributo in dic:
+                if dic[atributo] != "" and atributo != "":
+                    print atributo + "=" + dic[atributo] + "\t",
             print
 
     def do_local(self):     # descarga recursos remotos
         for sublista in self.lista:
             etiqueta = sublista[0]
-            sublista_atrib = sublista[1]
-            for atributo in sublista_atrib:
-                if sublista_atrib[atributo] != "" and atributo == "src":
-                    recurso = sublista_atrib[atributo]
+            dic = sublista[1]
+            for atributo in dic:
+                if dic[atributo] != "" and atributo == "src":
+                    recurso = dic[atributo]
                     os.system("wget -q " + recurso)
                     lista = recurso.split("/")
                     lista = lista[-1]
-                    sublista_atrib[atributo] = lista
-                    print sublista_atrib[atributo]
+                    dic[atributo] = lista
+                    print dic[atributo]
 
 if __name__ == "__main__":
     try:
-        smil = sys.argv[1]
-        karaoke = KaraokeLocal(smil)
+        karaoke = KaraokeLocal(sys.argv[1])
         karaoke.__str__()
         karaoke.do_local()
+        karaoke.__str__()
     except:
         print "Usage: python karaoke.py file.smil"
         sys.exit()
-# si quiero imprimir sin espacios entre el "=", me da usage error
